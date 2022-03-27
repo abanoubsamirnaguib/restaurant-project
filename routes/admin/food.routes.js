184d38ed8c7513cdd475b1cc1d345bb8062f2d11
Jolina Gerges <jolinagerges@gmail.com>
2022-03-28 01:45:13 +0200

    mm

---
+++ routes/admin/food.routes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const adminController = require("../../controller/admin/admin.controller");
+const foodController = require("../../controller/admin/food.controller");
+const userController = require("../../controller/web/user.controller");
+
+
+module.exports = router;
