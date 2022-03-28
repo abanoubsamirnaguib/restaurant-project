52ec8f2f9913e3edb7da18be943f28b054afe559
abanoubsamirnaguib <abanoubsamir@gmail.com>
2022-03-28 20:47:31 +0200

    food

--- routes/admin/food.routes.js
+++ routes/admin/food.routes.js
@@ -1,16 +1,7 @@
 const router = require("express").Router();
 const adminController = require("../../controller/admin/admin.controller");
-<<<<<<< HEAD
-// const foodController = require("../../controller/admin/food.controller");
+const foodController = require("../../controller/admin/food.controlller.js");
 const userController = require("../../controller/web/user.controller");
 
 
 module.exports = router;
-
-=======
-const foodController = require("../../controller/admin/food.controller");
-const userController = require("../../controller/web/user.controller");
-
-
-module.exports = router;
->>>>>>> d16660ee773be836377ab8ae8eb8fa7f9474f28d
