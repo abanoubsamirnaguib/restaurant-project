f3f5a6f4910477184b47afcfaee40bcf25214685
Jolina Gerges <jolinagerges@gmail.com>
2022-03-28 22:28:48 +0200

    m

--- controller/admin/food.controlller.js
+++ controller/admin/food.controlller.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express")
 const foodModel = require("../../db/models/food.model")
 
 class FOOD {
@@ -20,6 +21,84 @@ class FOOD {
         }
     }
     
+
+static edit = async (request,response)=> {
+    try {    const food = await foodModel.findByIdAndUpdate(
+        request.params.id, request.body, {runValidators:true}
+    )
+
+    
+    response.status(200).send({
+        apiStatus:true,
+        data:food,
+        message:"edit done"
+    })
+}
+catch(e){
+    response.status(500).send({
+        apiStatus:false,
+        errors:e.message,
+        message:"error in editing"
+    })
 }
 
+}
+
+static delet = async(request,response)=>{
+    try{
+        const food = await foodModel.findByIdAndDelete(request.params.id)
+        response.status(200).send({
+            apiStatus:true,
+            data:food,
+            message:"meal deleted"
+        })
+    }
+    catch(e){
+        response.status(500).send({
+            apiStatus:false,
+            errors:e.message,
+            message:"error in deleting"
+        })
+    }
+
+}
+
+static show = async (request,response)=> {
+    try {    const food = await foodModel.findById( request.params.id)
+    response.status(200).send({
+        apiStatus:true,
+        data:food,
+        message:" meal"
+    })
+}
+catch(e){
+    response.status(500).send({
+        apiStatus:false,
+        errors:e.message,
+        message:"error in load this meal"
+    })
+}
+
+}
+static showall = async (request,response)=> {
+    try { const food = new foodModel(request.body)
+        await foodModel
+    response.status(200).send({
+        apiStatus:true,
+        data: allfood,
+        message:" ALL MEALS"
+    })
+}
+catch(e){
+    response.status(500).send({
+        apiStatus:false,
+        errors:e.message,
+        message:"error in load meals"
+    })
+}
+
+}
+
+}
 module.exports = FOOD
+
--- routes/admin/food.routes.js
+++ routes/admin/food.routes.js
@@ -3,5 +3,10 @@ const adminController = require("../../controller/admin/admin.controller");
 const foodController = require("../../controller/admin/food.controller");
 const userController = require("../../controller/web/user.controller");
 
+router.post("/add",auth, foodController.add)
+router.get('/delete/:id', auth, foodController.delete)
+router.get('/edit/:id', auth, foodController.edit)
+router.get('/show/:id', auth, foodController.show)
+router.get('/showall', auth, foodController.showall)
 
 module.exports = router;
